62882819546f129092b73a8c9a920c8138050366
Eric Engle <EricEngle-NOAA@users.noreply.github.com>
2026-01-14 08:40:30 -0500

    MNT: Re-rendered with conda-smithy 3.54.1 and conda-forge-pinning 2026.01.13.21.47.52
    Other tools:
    - conda-build 25.9.0
    - rattler-build 0.48.1
    - rattler-build-conda-compat 1.4.9
    
    Re-enabling windows builds.

--- recipe/build_win.bat
+++ recipe/build_win.bat
@@ -1,13 +1,16 @@
+@echo on
+setlocal EnableDelayedExpansion
 
-mkdir build
-cd build
-
-cmake -G"Ninja" ^
+REM Configure
+cmake -S "%SRC_DIR%" -B build -G "Ninja" ^
       %CMAKE_ARGS% ^
       -DCMAKE_BUILD_TYPE=Release ^
-      -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-      -DCMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
-      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_LIBRARY_PATH="%LIBRARY_LIB%" ^
+      -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+      -DCMAKE_INSTALL_BINDIR=bin ^
+      -DCMAKE_INSTALL_LIBDIR=lib ^
+      -DCMAKE_INSTALL_INCLUDEDIR=include ^
       -DCMAKE_FIND_FRAMEWORK=NEVER ^
       -DCMAKE_FIND_APPBUNDLE=NEVER ^
       -DUSE_Jasper=ON ^
@@ -15,13 +18,13 @@ cmake -G"Ninja" ^
       -DUSE_PNG=ON ^
       -DUSE_AEC=ON ^
       -DBUILD_STATIC_LIBS=OFF ^
-      %SRC_DIR%
+      -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON
 if errorlevel 1 exit 1
 
-REM Build step
-cmake --build . --config Release
+REM Build
+cmake --build build --config Release --parallel
 if errorlevel 1 exit 1
 
-REM Install step
-cmake --build . --config Release --target install
+REM Install
+cmake --install build --config Release
 if errorlevel 1 exit 1
